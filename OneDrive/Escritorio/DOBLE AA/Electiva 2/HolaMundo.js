f681ac7e0463920adb25c79f202fdfe8dc93b860
AlexAlen07 <alexalenn29@gmail.com>
2025-02-11 17:48:41 -0400

    Arreglé un error en el sistema de inicio de sesión

---
+++ OneDrive/Escritorio/DOBLE AA/Electiva 2/HolaMundo.js
@@ -0,0 +1,6 @@
+function HolaMundo() {
+    return "Hola Mundo";
+}
+
+module.exports = HolaMundo;
+//Albert A. Peguero Garcia, 2023-1402
---
+++ OneDrive/Escritorio/DOBLE AA/Electiva 2/HolaMundo.test.js
@@ -0,0 +1,6 @@
+const HolaMundo = require("./HolaMundo");
+
+test("Debe retornar 'Hola Mundo!'", () => {
+    expect(HolaMundo()).toBe("Hola Mundo");
+});
+//Albert A. Peguero Garcia, 2023-1402
