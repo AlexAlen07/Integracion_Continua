4244a00535314eaa68696a8a3aa42c6be04e1972
AlexAlen07 <alexalenn29@gmail.com>
2025-02-11 22:52:55 -0400

    Agrega código de Hola Mundo, pruebas unitarias y flujo de trabajo de GitHub Actions

---
+++ Program.js
@@ -0,0 +1,6 @@
+function holaMundo(){
+    return "Hola Mundo!";
+}
+
+module.exports = holaMundo;
+//Albert Peguero Garcia, 2023-1402
---
+++ Program.test.js
@@ -0,0 +1,6 @@
+const holaMundo = require('./Program');
+
+test('Debe mostrar "Hola Mundo!"', () => {
+    expect(holaMundo()).toBe("Hola Mundo!");
+});
+//Albert Peguero Garcia, 2023-1402
